76af5922548820bb5e4007d01bdef64a1280c93f
dev-PC\dev <nadeera3784@gmail.com>
2020-07-29 15:33:24 -0700

    refactort : folder structure rearranged

--- app/helpers/language.js
+++ app/helpers/language.js
@@ -3,7 +3,7 @@ const path    = require('path');
 const languageHelper = async function () {
     let i18n = new (require('i18n-2'))({
         locales: ['en', 'de', 'es', 'ru'],
-        directory: path.join(__dirname, '../language/'),
+        directory: path.join(__dirname, '../../config/languages'),
         devMode : false
     });
     return i18n;
